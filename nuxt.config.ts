d8ca9d0e1749fbb3758a4298c9829de48aba0d10
Zayed Al Raeesi <ban.ad@hotmail.com>
2025-04-24 08:57:01 +0400

    removed tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,21 @@
-import tailwindcss from "@tailwindcss/vite";
-
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-04-03",
+
   devtools: { enabled: true },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/ui',
     '@pinia/nuxt'
   ],
 
   css: ['~/assets/css/main.css'],
 
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
 
   runtimeConfig: {
     // Private keys are only available on the server
     groqApiKey: process.env.GROQ_API_KEY,
-
-    // Public keys that are exposed to the client
-    public: {
-      // publicKey: process.env.PUBLIC_KEY
-    }
   }
 })
